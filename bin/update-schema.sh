af8fb2b40654ce0c95cb37b538610d9323216af3
Gunter Zeilinger <gunterze@gmail.com>
2016-05-05 10:13:32 +0200

    rename ldap/schema/update.ldif -> ldap/schema/dcm4chee-archive-modify.ldif

--- bin/update-schema.sh
+++ bin/update-schema.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ldapmodify -xw $LDAP_CONFIGPASS -D cn=admin,cn=config -f /etc/ldap/schema/update.ldif
+ldapmodify -xw $LDAP_CONFIGPASS -D cn=admin,cn=config -f /etc/ldap/schema/dcm4chee-archive-modify.ldif
