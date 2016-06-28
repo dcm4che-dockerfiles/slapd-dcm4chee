d17a25cf6c66b9881c3b4bab562353c46cde7763
Gunter Zeilinger <gunterze@gmail.com>
2016-06-28 10:51:19 +0200

    dcm4che/dcm4chee-arc-light#215: Provide ID Service to create sequential IDs according a configurable pattern

--- ldap/configure.sh
+++ ldap/configure.sh
@@ -13,6 +13,7 @@ if [ "$SKIP_INIT_CONFIG" != "true" ]; then
 			-e "s%dicomAETitle=DCM4CHEE%dicomAETitle=${AE_TITLE}%" \
 			-e "s%dicomAETitle: DCM4CHEE%dicomAETitle: ${AE_TITLE}%" \
 			-e "s%dcmRetrieveAET: DCM4CHEE%dcmRetrieveAET: ${AE_TITLE}%" \
+			-e "s%dcmRejectExpiredStudiesAETitle: DCM4CHEE%dcmRejectExpiredStudiesAETitle: ${AE_TITLE}%" \
 			-e "s%archive-host%${ARCHIVE_HOST}%" \
 			-e "s%11112%${DICOM_PORT}%" \
 			-e "s%2575%${HL7_PORT}%" \
