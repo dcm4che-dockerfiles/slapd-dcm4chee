ad7ce47d9aa5d8b4e9df729ce2865374952086ad
Gunter Zeilinger <gunterze@gmail.com>
2017-07-11 14:45:59 +0200

    Fix dcm4che/dcm4chee-arc-light#850: Docker images: Make Device Name for Audit Record Repository configurable by ENV SYSLOG_DEVICE_NAME

--- ldap/configure.sh
+++ ldap/configure.sh
@@ -16,9 +16,17 @@ if [ "$SKIP_INIT_CONFIG" != "true" ]; then
 			-e "s%dcmXDSiImagingDocumentSourceAETitle: DCM4CHEE%dcmXDSiImagingDocumentSourceAETitle: ${AE_TITLE}%" \
 			-e "s%dcmRejectExpiredStudiesAETitle: DCM4CHEE%dcmRejectExpiredStudiesAETitle: ${AE_TITLE}%" \
 			-e "s%archive-host%${ARCHIVE_HOST}%" \
+			-e "s%dicomDeviceName=unknown%dicomDeviceName=${UNKNOWN_DEVICE_NAME}%" \
+			-e "s%dicomDeviceName: unknown%dicomDeviceName: ${UNKNOWN_DEVICE_NAME}%" \
+			-e "s%dicomAETitle=UNKNOWN%dicomAETitle=${UNKNOWN_AE_TITLE}%" \
+			-e "s%dicomAETitle: UNKNOWN%dicomAETitle: ${UNKNOWN_AE_TITLE}%" \
+			-e "s%dicomDeviceName=keycloak%dicomDeviceName=${KEYCLOAK_DEVICE_NAME}%" \
+			-e "s%dicomDeviceName: keycloak%dicomDeviceName: ${KEYCLOAK_DEVICE_NAME}%" \
 			-e "s%keycloak-host%${KEYCLOAK_HOST}%" \
 			-e "s%11112%${DICOM_PORT}%" \
 			-e "s%2575%${HL7_PORT}%" \
+			-e "s%dicomDeviceName=logstash%dicomDeviceName=${SYSLOG_DEVICE_NAME}%" \
+			-e "s%dicomDeviceName: logstash%dicomDeviceName: ${SYSLOG_DEVICE_NAME}%" \
 			-e "s%syslog-host%${SYSLOG_HOST}%" \
 			-e "s%514%${SYSLOG_PORT}%" \
 			-e "s%SYSLOG_UDP%SYSLOG_${SYSLOG_PROTOCOL}%" \
